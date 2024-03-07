b247fd0910f6bf22804dac98d039b0b751fd11aa
Rocoso <corbaroco@gmail.com>
2024-03-06 23:46:35 -0300

    Add function to get player's choice
    Make getPlayerChoice to prompt the user for an
    input, if it's invalid asks again. If it's valid
    make it lower case and return it.

--- scripts/game.js
+++ scripts/game.js
@@ -1,14 +1,28 @@
-console.log(getComputerChoice());
+getPlayerChoice();
+
+function getPlayerChoice(){
+    let playerChoice = prompt("Choose ROCK PAPER OR SCISSORS");
+    playerChoice = playerChoice.toLowerCase();
+    if (playerChoice == "rock" || playerChoice == "paper" || playerChoice == "scissors" ){
+        alert(`You picked ${playerChoice}`)
+        console.log(playerChoice);
+        return playerChoice;
+    }else {
+        alert("what?")
+        console.log(playerChoice);
+        getPlayerChoice();
+    }
+}
 
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * (Math.floor(3) - Math.ceil(1) + 1)) + Math.ceil(1);
     switch(choice) {
         case 1:
-            return "Rock";
+            return "rock";
         case 2:
-            return "Paper";
+            return "paper";
         case 3:
-            return "Scissors";
+            return "scissors";
     }
 
 }
